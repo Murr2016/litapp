e22c0d0f5a31dc95d2ab9ba5ff0515534ebceb26
laeubli <s.laeubli@me.com>
2016-07-20 21:27:44 +0200

    change temp name of uploaded file

--- litapp/import.php
+++ litapp/import.php
@@ -3,7 +3,7 @@ require_once("res/Funktionen.php");
 
 // Process uploaded file (if any)
 if(isset($_POST["submit"])) {
-    echo $_FILES['uploadedfile']['tmp_name'];
+    echo $_FILES['uploadedFile']['tmp_name'];
 }
 ?>
 <!DOCTYPE html>
@@ -12,7 +12,7 @@ if(isset($_POST["submit"])) {
 
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
     Select SQL Script to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="file" name="uploadedFile" id="uploadedFile">
     <input type="submit" value="Upload SQL Script" name="submit">
 </form>
 
