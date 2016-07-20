e9a2dc6690bdd84c2c8a10998b9fa618d126a5e1
laeubli <s.laeubli@me.com>
2016-07-20 15:19:03 +0200

    add default db schema

--- litapp/res/settings.php
+++ litapp/res/settings.php
@@ -1,7 +1,7 @@
 <?
-/********** 
+/**********
 **
-** In dieser Datei werden die Parameter gesetzt. 
+** In dieser Datei werden die Parameter gesetzt.
 **
 *****/
 
@@ -11,7 +11,7 @@
 
 define ('ANZAHL_BAEUME', 0); //0 = mehrere Bäume; 1 = nur ein Baum.
 
-//// 
+////
 // DATENBANKSETTINGS
 ///
 
@@ -19,13 +19,13 @@ error_reporting(E_ALL);
 
 // Zum Aufbau der Verbindung zur Datenbank
 define ( 'MYSQL_HOST', 'localhost' );
- 
+
 // bei XAMPP ist der MYSQL_Benutzer: root
 define ( 'MYSQL_BENUTZER',  'root' );
 define ( 'MYSQL_KENNWORT',  '' );
 // für unser Bsp. nennen wir die DB adressverwaltung
-define ( 'MYSQL_DATENBANK', 'CadavreExquis' );
-define ('MYSQL_TABELLE', 'sentences'); 
+define ( 'MYSQL_DATENBANK', 'LitAppDB' );
+define ( 'MYSQL_TABELLE', 'sentences');
 
 
 ////
---
+++ litapp/schema/default.sql
@@ -0,0 +1,52 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+01:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `LitAppDB`
+--
+
+CREATE DATABASE IF NOT EXISTS `LitUpDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci
+USE `LitUpDB`
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `Sentences`
+--
+
+CREATE TABLE `Sentences` (
+  `ID` int(11) NOT NULL,
+  `ID_Parent` int(11) NOT NULL,
+  `HasChildren` boolean(1) DEFAULT '0',
+  `Text` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `Sentences`
+--
+ALTER TABLE `Sentences`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ID` (`ID`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `Sentences`
+--
+ALTER TABLE `Sentences`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
